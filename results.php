ba99e4ef696a4856e9b3e0e5aeb36354d694364d
Enver Arco <oisinmacsweeney@hotmail.com>
2019-03-16 16:48:41 +0000

    Add files via upload

---
+++ results.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["first_name"]; ?><br>
+Welcome <?php echo $_POST["first_name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
