7b93c15d1be7a4df781049b4b407b69a90c0094a
Enver Arco <oisinmacsweeney@hotmail.com>
2019-03-16 16:51:47 +0000

    Add files via upload

---
+++ simpleform.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Skype: <input type="text" name="skype"><br>
+Address: <input type="text" name="address"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
