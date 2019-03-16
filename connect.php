f4163f2ed8cfcfd750c359e4c1301577000eda4a
Dan Quinn <quinnd6@tcd.ie>
2019-03-16 22:01:04 +0000

    added a function to create inputs

--- connect.php
+++ connect.php
@@ -1,12 +1,15 @@
 <?php
 
-$link = mysqli_connect("oisincode.offyoucode.co.uk", "oisincod_task", 'r*r_QP)by)dz', "oisincod_users");
-var_dump($link);
-if(mysqli_connect_error()){
-	echo "things";
-}
-else {
-	echo "success";
+$DB_HOST = "localhost";
+$DB_USER = "s2995020";
+$DB_PWD = "halierse";
+$DB_NAME = "s2995020";
+
+$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PWD, $DB_NAME) or die("Error: " . mysqli_connect_error());
+
+mysqli_set_charset($conn, 'utf8');
+
+if($conn){
+	echo "Success";
 }
 
-?>
