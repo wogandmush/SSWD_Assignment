e4f9f97680a7985fbcbf0b8e3b60f279d576411c
Enver Arco <oisinmacsweeney@hotmail.com>
2019-03-17 21:41:41 +0000

    Add files via upload

---
+++ register.php
@@ -0,0 +1,355 @@
+<?php
+
+require '../config/connect.php';
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// primary validate function
+	function validate($str) {
+		return trim(htmlspecialchars($str));
+	}    
+	           
+	
+    if (empty($_POST['userno'])) {
+		$usernoError = 'userno should be filled';
+	} else {
+		$userno = validate($_POST['userno']);
+         if (!preg_match('/^[0-9]{0,15}+$/', $userno)) {
+        $usernoError = 'userno can only contain numbers'; } 
+     
+	}
+    
+	if (empty($_POST['name'])) {
+		$nameError = 'Name should be filled';
+	} else {
+		$name = validate($_POST['name']);
+		if (!preg_match('/^[a-zA-Z0-9\s]+$/', $name)) {
+			$nameError = 'Name can only contain letters, numbers and white spaces';
+		}
+	}
+    
+    if (empty($_POST['lname'])) {
+		$lnameError = 'Last Name should be filled';
+	} else {
+		$lname = validate($_POST['lname']);
+		if (!preg_match('/^[a-zA-Z0-9\s]+$/', $lname)) {
+			$lnameError = 'Name can only contain letters, numbers and white spaces';
+		}
+	}
+
+    if (empty($_POST['dateofbirth'])) {
+		$dateofbirthError = 'date of birth should be filled';
+	} else {
+		$dateofbirth = validate($_POST['dateofbirth']);
+            
+            $temp_date = $dateofbirth;
+            $date_arr  = explode('/', $temp_date);
+            if (checkdate($date_arr[0], $date_arr[1], $date_arr[2])) {
+                $dateofbirthError = 'date of birth must be written in the correct format';
+            }         	
+	}
+
+    
+    if (empty($_POST['gender'])) {
+		$genderError = 'Please enter your gender';
+	} else {
+		$gender = $_POST['gender'];
+	}
+
+
+
+    if (empty($_POST['phoneno'])) {
+		$phonenoError = 'phone number should be filled and match the following format eg 0852534837';
+	} else {
+		$phoneno = validate($_POST['phoneno']);
+		if (!preg_match('/^[0-9]{10}+$/', $phoneno)) {
+			$phonenoError = 'phone number should only contain numbers and match the following format eg 0852534837';
+		}
+	}
+        
+         if (empty($_POST['mobilephoneno'])) {
+		$mobilephonenoError = 'mobile phone number should be filled and match the following format eg 0852534837 ';
+	} else {
+		$mobilephoneno = validate($_POST['mobilephoneno']);
+		if (!preg_match('/^[0-9]{10}+$/', $mobilephoneno)) {
+			$mobilephonenoError = 'mobile phone number should only contain numbers and match the following format eg 0852534837';
+		}
+	}
+
+
+
+	if (empty($_POST['email'])) {
+		$emailError = 'Please enter your email';
+	} else {
+		$email = validate($_POST['email']);
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$emailError = 'Invalid Email';
+		}
+	}
+     
+    
+      if (empty($_POST['address'])) {
+		$addressError = 'address cannot be empty';
+	} else {
+		$address = validate($_POST['address']);
+		if (strlen($address) < 10) {
+			$addressError = 'Address should be longer than 10 characters';
+		}
+	}
+    
+	if (empty($_POST['password'])) {
+		$passwordError = 'Password cannot be empty';
+	} else {
+		$password = validate($_POST['password']);
+		if (strlen($password) < 6) {
+			$passwordError = 'Password should be longer than 6 characters';
+		}
+	}
+    
+    if (empty($_POST['membership'])) {
+		$membershipError = 'Please enter your membership';
+	} else {
+		$membership = $_POST['membership'];
+	}
+    
+	$remember = !empty($_POST['remember']) ? filter_var($_POST['remember'], FILTER_VALIDATE_BOOLEAN) : ""; 
+
+	if (empty($nameError) && empty($emailError) && empty($genderError) && empty($passwordError) && empty($lnameError) && empty($dateofbirthError) && empty($phonenoError) && empty($mobilephonenoError) && empty($usernoError) && empty($addressError) && empty($membershipError)) 
+    {
+		// completed the form
+		?>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Complete Form</title>
+                <style type="text/css">
+                    .error {
+                        color:red;
+                    }
+                </style>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/materia/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        </head>
+        <body>
+            <div class="text-center">
+                <h1><strong><br>You have filled the form successfully!</strong></h1>
+                <h5><strong><br><br>Your user number is </strong></h5> <?php echo $userno ?><br>         
+                <h5><strong>Your first name is </strong></h5> <?php echo $name ?><br>  
+                <h5><strong>Your second name is </strong></h5> <?php echo $lname ?><br>   
+                <h5><strong>Your birthdate is </strong></h5> <?php echo $dateofbirth ?><br> 
+                <h5><strong>Your Gender is </strong></h5> <?php echo $gender ?><br>  
+                <h5><strong>Your mobile number is </strong></h5> <?php echo $mobilephoneno ?><br>   
+                <h5><strong>Your phone number is </strong></h5> <?php echo $phoneno ?><br>     
+                <h5><strong>Your email is </strong></h5> <?php echo $email ?><br>       
+                <h5><strong>Your Address is </strong></h5> <?php echo $address ?><br>
+                <h5><strong>Your password is </strong></h5> <?php echo $password ?><br>
+                <h5><strong>Your membership is </strong></h5> <?php echo $membership ?><br>
+                
+          </div>                       
+        </body>
+
+
+
+        <?php
+            
+        $sql = "INSERT INTO members (user_no, first_name, last_name, date_of_birth, gender, mobile, home_tel, email, address, membership, password) VALUES ('$userno', '$name', '$lname', '$dateofbirth', '$gender', '$mobilephoneno', '$phoneno', '$email', '$address', '$membership', '$password')";
+        if(mysqli_query($conn, $sql)){
+            echo "<p>New row added successfully!</p>";  
+        }else{
+        echo "ERROR: Unable to execute $sql" . mysql_error($conn);   
+        }     
+            
+		exit(); // terminates the script
+	} 
+    
+}
+
+?>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Complete Form</title>
+        <style type="text/css">
+            .error {
+                color:red;
+            }
+        </style>
+    <!-- Materia -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/materia/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+	
+	
+	
+	
+    <div class="container"> 
+    <h2><br>Member Registration</h2><br><br>
+<form class="form-horizontal" action="" method="post">
+    <!--
+	<div class="col-xs-1" align="left">
+	<button type="button" onclick="location.href='https://knuth.griffith.ie/~s2994354/lab5/add_search.php'" class="btn btn-warning">Go Back</button>
+	
+	<button type="button" onclick="location.href='https://knuth.griffith.ie/~s2994354/lab5/search.php'" class="btn btn-info">Search</button><br><br>
+	</div>
+	-->
+	
+	
+	
+     <div class="form-group">
+      <label class="control-label col-sm-2" for="userno">User Number</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" placeholder="Enter Student Number eg 125" name="userno" value="<?php if (isset($userno)) echo $userno ?>"/>
+      </div>
+         <span class="error"><?php if (isset($usernoError)) echo $usernoError ?></span><br>
+    </div>
+	
+  
+    
+    <div class="form-group"> 
+        <label class="control-label col-sm-2" for="name">First Name *</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" name="name" placeholder="Enter First Name" value="<?php if (isset($name)) echo $name ?>" pattern="^[( )a-zA-Z]+$"/>
+        </div>
+        <span class="error"><?php if (isset($nameError)) echo $nameError ?></span><br>
+    </div>  
+    
+    <div class="form-group"> 
+        <label class="control-label col-sm-2" for="lname">Last Name *</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" name="lname" placeholder="Enter Last Name" value="<?php if (isset($lname)) echo $lname ?>" pattern="^[( )a-zA-Z]+$"/>
+        </div>
+        <span class="error"><?php if (isset($lnameError)) echo $lnameError ?></span><br>
+    </div>  
+    
+    <div class="form-group"> 
+        <label class="control-label col-sm-2" for="name">Email *</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" name="email" placeholder="Enter Email eg johnsmith@gmail.com" value="<?php if (isset($email)) echo $email ?>" pattern="^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$"/>
+        </div>
+        <span class="error"><?php if (isset($emailError)) echo $emailError ?></span><br>
+    </div>    
+    
+    <div class="form-group"> 
+        <label class="control-label col-sm-2" for="password">Password *</label>
+        <div class="col-sm-10">
+            <input type="password" class="form-control" name="password" placeholder="Enter password" value="<?php if (isset($password)) echo $password ?>"/>
+        </div>
+        <span class="error"><?php if (isset($passwordError)) echo $passwordError ?></span><br>
+    </div>  
+    
+    
+     <div class="form-group"> 
+        <label class="control-label col-sm-2" for="phoneno">Phone Number *</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" name="phoneno" placeholder="Enter phone number eg 0852534837" value="<?php if (isset($phoneno)) echo $phoneno ?>"/>
+        </div>
+        <span class="error"><?php if (isset($phonenoError)) echo $phonenoError ?></span><br>
+    </div>  
+    
+    <div class="form-group"> 
+        <label class="control-label col-sm-2" for="mobilephoneno">Mobile Number *</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" name="mobilephoneno" placeholder="Enter mobile phone number" value="<?php if (isset($mobilephoneno)) echo $mobilephoneno ?>"/>
+        </div>
+        <span class="error"><?php if (isset($mobilephonenoError)) echo $mobilephonenoError ?></span><br>
+    </div>  
+    
+    <div class="form-group"> 
+        <label class="control-label col-sm-2" for="address">Address *</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" name="address" placeholder="Enter address" value="<?php if (isset($address)) echo $address ?>"/>
+        </div>
+        <span class="error"><?php if (isset($addressError)) echo $addressError ?></span><br>
+    </div>  
+    
+    <div class="form-group"> 
+        <label class="control-label col-sm-2" for="dateofbirth">Date of Birth *</label>
+        <div class="col-sm-10">
+            <input type="date" class="form-control" name="dateofbirth" placeholder="Enter date of birth" value="<?php if (isset($dateofbirth)) echo $dateofbirth ?>"/>
+        </div>
+        <span class="error"><?php if (isset($dateofbirthError)) echo $dateofbirthError ?></span><br>
+    </div>  
+    
+    
+    <div class="wow fadeInDown form-group" data-wow-delay="0.9s"> 
+        <label class="control-label col-sm-2" for="select">Membership *</label>
+        <select class="browser-default custom-select col-sm-10" id="select" name="membership">
+        <option value="" disabled="" selected="" >Choose your membership option </option>  
+        <option value="Student Monthly" >Student Monthly (€29.50 monthly)</option>
+        <option value="Adult Monthly">Adult Monthly (€39.50 monthly)</option>
+        <option value="Adult Yearly">Adult Yearly (€379.50 yearly)</option>
+     </select>
+        <br><span class="error"><?php if (isset($membershipError)) echo $membershipError ?></span><br>
+    </div><br><br>
+        
+    
+	<!--Password: 
+	<input type="password" name="password"> 
+	<span class="error"><?php// if (isset($passwordError)) echo $passwordError ?></span><br><br> -->
+    
+                   <!-- Default inline 1-->
+    <p>Which Classes would you like to sign up to</p>
+
+    <div class="custom-control custom-checkbox custom-control-inline">
+      <input type="checkbox" class="custom-control-input" id="defaultInline1">
+      <label class="custom-control-label" for="defaultInline1">Strength and Stretch</label>
+    </div> <br><br>
+
+    <!-- Default inline 2-->
+    <div class="custom-control custom-checkbox custom-control-inline">
+      <input type="checkbox" class="custom-control-input" id="defaultInline2">
+      <label class="custom-control-label" for="defaultInline2">Yoga</label>
+    </div> <br><br>
+
+    <!-- Default inline 3-->
+    <div class="custom-control custom-checkbox custom-control-inline">
+      <input type="checkbox" class="custom-control-input" id="defaultInline3">
+      <label class="custom-control-label" for="defaultInline3">Spin</label>
+    </div> <br><br>
+
+                            <!-- Default inline 1-->
+    <div class="custom-control custom-checkbox custom-control-inline">
+      <input type="checkbox" class="custom-control-input" id="defaultInline4">
+      <label class="custom-control-label" for="defaultInline4">Upper Body</label>
+    </div> <br><br>
+
+    <!-- Default inline 2-->
+    <div class="custom-control custom-checkbox custom-control-inline">
+      <input type="checkbox" class="custom-control-input" id="defaultInline5">
+      <label class="custom-control-label" for="defaultInline5">Lower Body</label>
+    </div> <br><br>
+
+    <!-- Default inline 3-->
+    <div class="custom-control custom-checkbox custom-control-inline">
+      <input type="checkbox" class="custom-control-input" id="defaultInline6">
+      <label class="custom-control-label" for="defaultInline6">Cardio</label>
+    </div><br><br> <br><br>
+      
+        
+    <div class="form-group">   
+        Gender: 
+		Male
+		<input type="radio" name="gender" value="male" <?php if (isset($gender) && $gender === "male") echo "checked" ?>> 
+		Female
+		<input type="radio" name="gender" value="female" <?php if (isset($gender) && $gender === "female") echo "checked" ?>><br>
+        <span class="error"><?php if (isset($genderError)) echo $genderError ?></span> <br>   
+     </div> <br><br> 
+        
+	<!--Remember Me: 
+	<input type="checkbox" name="remember"> -->
+    <button class="btn btn-outline-success" type="submit">Submit</button>
+     
+        
+        
+    
+</form>
+         </div>
+</body>
+</html>
---
+++ showtables.php
@@ -0,0 +1,80 @@
+<?php
+
+require '../config/connect.php';
+
+	?>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Complete Form</title>
+                <style type="text/css">
+                    .error {
+                        color:red;
+                    }
+                </style>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/materia/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        </head>
+        <body>
+       
+                
+                           
+        </body>
+
+        <?php
+       
+        $sql = "SELECT * FROM members";
+if($result = mysqli_query($conn, $sql)){
+   // print_r($result); 
+    if(mysqli_num_rows($result)>0){
+        echo "<table class='table table-stripped table-hover table-condensed table-bordered'>
+                <tr>
+                <th>user_no</th>
+                <th>first_name</th>
+                <th>last_name</th>
+                <th>date_of_birth</th>
+                <th>gender</th>
+                <th>mobile</th>
+                <th>home_tel</th>
+                <th>email</th>
+                <th>address</th>
+                <th>membership</th>
+                <th>password</th>         
+                </tr>
+        ";
+        
+//        $count = 0;
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+//            $count++;   
+//            echo "<p>Row number: $count</p>";
+//            print_r($row);
+            echo "<tr>";
+                echo "<td>" . $row["user_no"] . "</td>";
+                echo "<td>" . $row["first_name"] . "</td>";
+                echo "<td>" . $row["last_name"] . "</td>";
+                echo "<td>" . $row["email"] . "</td>";
+                echo "<td>" . $row["gender"] . "</td>";
+                echo "<td>" . $row["mobile"] . "</td>";
+                echo "<td>" . $row["home_tel"] . "</td>";
+                echo "<td>" . $row["email"] . "</td>";
+                echo "<td>" . $row["address"] . "</td>";
+                echo "<td>" . $row["membership"] . "</td>";
+                echo "<td>" . $row["password"] . "</td>";
+            echo "</tr>";
+            
+        }
+        echo "</table>";
+        //close the result set
+        mysqli_free_result($result);
+    }else{  
+        ?>
+        <h4>Error with the table</h4>
+        <?php
+    }
+}else{
+    echo "<p>Unable to excecute: $sql. " . mysqli_error($conn) ."</p>";   
+}
+
+?>
+
