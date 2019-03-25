fc14cf5b77cd41997cbdad2b16a6f2bc08bc2666
wogandmush <quinnd6@tcd.ie>
2019-03-25 18:46:58 +0000

    Email working

--- mail.php
+++ mail.php
@@ -2,16 +2,16 @@
 if(isset( $_POST['name']))
 $name = $_POST['name'];
 if(isset( $_POST['email']))
-$email = $_POST['email'];
+$recipient = $_POST['email'];
 if(isset( $_POST['message']))
 $message = $_POST['message'];
 if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
+$email = "swole@fit.ie";
 
 $content="From: $name \n Email: $email \n Message: $message";
 $mailheader = "From: $email \r\n";
 
-$recipient = "quinnd6@tcd.ie";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
 ?>
