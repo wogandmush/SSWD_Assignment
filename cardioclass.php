5339b441bae520335ee5eb2a94d477d675c4a902
Enver Arco <oisinmacsweeney@hotmail.com>
2019-04-09 04:37:56 +0100

    Add files via upload
    Added drop down navigation to class page with class pages selectable from dropdown

---
+++ cardioclass.php
@@ -0,0 +1,66 @@
+<?php 
+include 'header.php'; 
+#^^this should be the first line of the file. Body tags are already opened
+?>
+
+    <!-- Hero Image -->
+    <div class="jumbotron text-center">
+
+      <!-- Title -->
+      <!--<h2><strong>Classes</strong></h2>-->
+
+      <!-- Card image -->
+      <div class="view overlay my-4">
+
+        <img src="https://blogcms.gymshark.com/uploads/1540458284-strength-or-cardio-header.jpg" height="600" width="1280">
+        <a href="#"></a>
+      </div>
+
+    <div class="container">
+     <h1 class="h1-responsive font-weight-bold text-center my-5">Cardio</h1>
+        <p class="lead grey-text text-center w-responsive mx-auto mb-5"> The ultimate high-energy workout class that uses martial arts techniques inspired by Taekwondo, Kung Fu, Tai Chi and Karate. Increase your mobility and flexibility whilst burning calories and boosting metabolism in this fun-filled high energy class. </p>
+
+    </div>
+
+    <body>
+
+        <!-- Section: Features v.2 -->
+<section class="my-5">
+
+  <!-- Section heading -->
+    <div class="container">
+  <h2 class="h1-responsive font-weight-bold text-center my-5">What is Cardio?</h2>
+  <!-- Section description font-weight-bolder-->
+  <p class="lead grey-text text-center w-responsive mx-auto mb-5">This is a full body interval training programme that burns the maximum number of calories. It is suitable for people with a moderate fitness level and incorporates exercises such as high knees, march twists, upper cuts and knee-to-elbows. And you don’t need to have any prior experience in martial arts to join in.
+
+Cardio class at Perfect Form Fitness is an alternative cardio exercise that teaches you how to kick, jab and clinch in a non-contact environment. It’s a fast paced class that keeps your heart racing and really makes you sweat. Classes can be quite intense so we recommend you take it at your own pace and build endurance over time.
+
+At the end of each Cardio session you will leave exhausted but feeling great. Complete beginners will feel the burn for a few days after their first few sessions. This challenging mix of martial arts and cardio is a great way to unleash your inner strength! </p>
+    </div>
+    
+     <!-- Section heading -->
+    <div class="container">
+  <h2 class="h1-responsive font-weight-bold text-center my-5">What are the Benifits of Cardio Class</h2>
+  <!-- Section description -->
+  <p class="lead grey-text text-center w-responsive mx-auto mb-5">Our Perfect Form Fitness instructors will show you techniques to help burn fat. An hour Cardio class allows you to burn up to 1,000 calories. Although individual results vary depending on your fitness level, height and weight, it is a class that will certainly keep you on your toes.
+
+This empowering cardio workout will also help you build muscle, improve balance and coordination. It will work your upper body, lower body and core. Toning your arms, abs and legs. It will also boost your energy levels and is a great way to de-stress.</p>
+        <a href="" class="btn btn-success btn-md">Sign Up</a>
+    </div>
+    
+    
+   
+    
+
+</section>
+<!-- Section: Features v.2 -->
+        
+        
+
+
+    </body>    
+        
+<?php
+#vv this should be the last line. body and html tags are closed
+include 'footer.php';
+?>
---
+++ strengthclass.php
@@ -0,0 +1,67 @@
+<?php 
+include 'header.php'; 
+#^^this should be the first line of the file. Body tags are already opened
+?>
+
+    <!-- Hero Image -->
+    <div class="jumbotron text-center">
+
+      <!-- Title -->
+      <!--<h2><strong>Classes</strong></h2>-->
+
+      <!-- Card image -->
+      <div class="view overlay my-4">
+
+        <img src="https://www.lesmills.com.au/sites/default/files/styles/web_header/public/2017-02/bodyattack_class1.jpg?itok=vYptkEKg" height="600" width="1280">
+        <a href="#"></a>
+      </div>
+
+     <div class="container">
+     <h1 class="h1-responsive font-weight-bold text-center my-5">Strength</h1>
+        <p class="lead grey-text text-center w-responsive mx-auto mb-5"> Keep fit and future proof your body with mobility, weights and bodyweight exercises. Strength Class is designed to meet the needs of those who are over 50. </p>
+
+    </div>
+
+    <body>
+
+        <!-- Section: Features v.2 -->
+<section class="my-5">
+
+  <!-- Section heading -->
+    <div class="container">
+  <h2 class="h1-responsive font-weight-bold text-center my-5">What is Strength Class?</h2>
+  <!-- Section description font-weight-bolder-->
+  <p class="lead grey-text text-center w-responsive mx-auto mb-5">As we age, it is critical to stay active. We should listen to the needs and abilities of our bodies, to reduce the risk of injury or strain. You will work with trained Perfect Form Fitness instructors, who can modify exercises to suit your individual needs and fitness abilities.
+
+Also, Strength class is for anyone who is looking for an exercise that is fun, friendly and supportive. you will use your own body weight as well as kettlebells and dumbbells. Each class has a different focus, helping target problem areas as needed. </p>
+    </div>
+    
+     <!-- Section heading -->
+    <div class="container">
+  <h2 class="h1-responsive font-weight-bold text-center my-5">What are the Benifits of Strength Class</h2>
+  <!-- Section description -->
+  <p class="lead grey-text text-center w-responsive mx-auto mb-5">While most older adults regularly walk, swim or run for exercise many do not perform weight or resistance training. Strength class will help you build muscle, increase mobility and improve bone density. All of which are important areas to focus on as we age.
+
+You can achieve better balance and reduce the risk of injury or strain through resistance training.  Instructors will work with members to modify exercises as required and ensure they meet individual needs. Each week you will feel stronger and build endurance, enabling you to perform other aerobic activities.
+
+Strength Class is an enjoyable class. With a friendly atmosphere led by enthusiastic instructors. Moreover, it’s a great way to meet new people and socialise. You can easily chat and interact with other class members, helping each other to stay motivated.</p>
+        <a href="" class="btn btn-success btn-md">Sign Up</a>
+    </div>
+    
+    
+   
+    
+
+</section>
+<!-- Section: Features v.2 -->
+        
+        
+
+
+    </body>    
+        
+<?php
+#vv this should be the last line. body and html tags are closed
+include 'footer.php';
+?>
+
