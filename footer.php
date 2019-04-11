e9aba4e267bdfe31419cbc1071fcbc22fe4abdfd
Daniel Quinn Ballard <quinnd6@tcd.ie>
2019-04-11 18:52:53 +0000

    whyyyy

--- footer.php
+++ footer.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Footer</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-     
- <!--  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
-   
-    
-    <!--
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.min.css">
--->
-    
-    <link rel="stylesheet" href="css/Footer.css">
-</head>
-
 <body>
 <div class="content">
 </div>
