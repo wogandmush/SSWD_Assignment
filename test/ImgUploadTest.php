717797f0f4f67952e9111f911a9c75502d430d18
Daniel Quinn Ballard <quinnd6@tcd.ie>
2019-04-14 18:19:41 +0000

    Created draft of image cropping tool

--- test/ImgUploadTest.php
+++ test/ImgUploadTest.php
@@ -25,9 +25,55 @@ var cxt = canvas.getContext("2d");
 var upload = new Image();
 
 upload.onload = function(){
-	cxt.drawImage(upload, 0, 0);
+	var selection = {
+		x: 0,
+		y: 0,
+		w: 600,
+		h: 600,
+		clear: function(c){
+			c.clearRect(this.x, this.y, this.w, this.h);
+		}
+	}
+	var overlay = document.createElement("canvas");
+	var overlayCxt = overlay.getContext("2d");
+	overlayCxt.fillStyle = "grey";
+	overlayCxt.globalAlpha = 0.5;
+	width = upload.naturalWidth;
+	height = upload.naturalHeight;
+	canvas.width =  width;
+	canvas.height = height;
+	overlay.width = width;
+	overlay.height = height;
+
+	
+	function draw(){
+		overlayCxt.clearRect(0, 0, width, height);
+		overlayCxt.fillStyle = "grey";
+		overlayCxt.globalAlpha = 0.5;
+		overlayCxt.fillRect(0, 0, width, height);
+		selection.clear(overlayCxt);
+		cxt.drawImage(upload, 0, 0);
+		cxt.drawImage(overlay, 0, 0);
+	}
+	canvas.addEventListener("click", e => {
+		Object.assign(selection, {
+			x: (e.pageX - canvas.offsetLeft) - selection.w / 2,
+			y: (e.pageY - canvas.offsetTop) - selection.h / 2
+		});
+		if(selection.x < 0)
+			selection.x = 0;
+		else if(selection.x + selection.w > width)
+			selection.x = width - selection.w;
+		if(selection.y < 0)
+			selection.y = 0;
+		else if(selection.y + selection.h > height)
+			selection.y = height - selection.h;
+		draw();
+	});
+	draw();	
 }
-upload.src = "<?php echo $filepath;?>";
+
+upload.src = "<?php echo $root . "/images/temp/".$_FILES['img-upload']['name'];?>";
 
 	</script>
 <?php
