295dc6da06d8ef7224df3760b87a1ede64ee0de2
martinfeeley <martincfeeley@yahoo.com>
2019-04-24 11:40:23 +0100

    classdeck component for insertion into index page

---
+++ classdeck.php
@@ -0,0 +1,87 @@
+<!--Gym Classes Card-deck-->
+    <div class="container"><br><br><br><br><br><br>
+        <!-- Card deck -->
+        <div class="card-deck">
+
+          <!-- Card -->
+          <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view overlay">
+              <!--<img class="card-img-top" src="images/feature1.jpg" alt="Card image cap">-->
+             <img  src= "https://cdn.pixabay.com/photo/2018/01/01/01/56/yoga-3053487_960_720.jpg" alt="" height="260" width="350">
+              <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+              <!--Title-->
+              <h4 class="card-title">Yoga</h4>
+              <!--Text-->
+              <p class="card-text">Yoga Classes for all levels whether you're a vetern or beginner our classes will leave you feeling rejuvenated and stress free.</p>
+              <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <a href="yogaclass.php" class="btn btn-info btn-md">Read more</a>
+
+            </div>
+
+          </div>
+          <!-- Card -->
+
+            <!-- Card -->
+          <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view overlay">
+              <!--<img class="card-img-top" src="images/feature1.jpg" alt="Card image cap">-->
+             <img  src= "https://cdn.pixabay.com/photo/2017/04/25/20/18/sport-2260736_960_720.jpg" alt="" height="260" width="350">
+              <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+              <!--Title-->
+              <h4 class="card-title">Strength</h4>
+              <!--Text-->
+              <p class="card-text">Strength Class is a brand new fitness class which will build your core strength and help prevent injury, imporve joint mobility and resilience</p>
+              <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+              <a href="strengthclass.php" class="btn btn-info btn-md">Read more</a>
+
+            </div>
+
+          </div>
+          <!-- Card -->
+
+          <!-- Card -->
+          <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view overlay">
+              <!--<img class="card-img-top" src="images/feature1.jpg" alt="Card image cap">-->
+             <img  src= "https://blog.nasm.org/wp-content/uploads/2017/05/201705_metabolic-conditioning.jpg" alt="" height="260" width="350">
+              <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+              <!--Title-->
+              <h4 class="card-title">Cardio</h4>
+              <!--Text-->
+              <p class="card-text">Feel the burn with an intense cardio workout that'll leave you in much better shape and boost overall fitness levels. </p>
+              <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+              <a href="cardioclass.php" class="btn btn-info btn-md">Read more</a>
+
+            </div>
+          </div>
+        </div>
+      <!-- Card -->
+
+    </div>
